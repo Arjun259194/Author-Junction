e9dcc241186602910bdd9e965379d234d556e319
arjun259194 <arjun259194@gmail.com>
2023-04-07 14:12:15 +0530

    api endpoint and client side function to create post ready and tested

---
+++ pages/api/post/index.ts
@@ -0,0 +1,36 @@
+import { createPost } from "@/database/operations";
+import connectDB from "@/utils/api/connectDB";
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case "POST":
+      return postHandler(req, res);
+    default:
+      return res.status(405).json({ message: "Http method not available" });
+  }
+}
+
+async function postHandler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await connectDB();
+
+    const { title, content } = req.body as {
+      title: string;
+      content: string;
+    };
+
+    const token = req.cookies.accessToken;
+
+    if (!token) return res.status(401).json({ message: "unauthorized" });
+    const payload = jwt.decode(token) as jwt.JwtPayload;
+    const id = payload.id;
+    const post = createPost({ title, content, creator: id });
+    await post.save();
+    return res.status(200).json({ message: "post created 👍" });
+  } catch (err) {
+    console.log(err);
+    return res.status(502).json({ error: err });
+  }
+}
--- utils/apiClient.ts
+++ utils/apiClient.ts
@@ -1,47 +1,46 @@
-type HttpMethods = "GET" | "POST" | "DELETE" | "PUT"
+import { Post } from "@/database/model/Post";
+
+type HttpMethods = "GET" | "POST" | "DELETE" | "PUT";
 
 export default class API {
-  private getHeader(): Headers {
-    const header = new Headers()
-    header.append("Content-Type", "application/json")
-    return header
+  private header: Headers;
+
+  constructor() {
+    this.header = new Headers();
+    this.header.append("Content-Type", "application/json");
   }
 
-  private getFetchOptions(
-    header: Headers,
-    method: HttpMethods,
-    body?: { [key: string]: any }
-  ): RequestInit {
-    return body
-      ? { method: method, body: JSON.stringify(body), headers: header }
-      : { method: method, headers: header }
+  private getFetchOptions(method: HttpMethods, body?: { [key: string]: any }): RequestInit {
+    return body ? { method: method, body: JSON.stringify(body), headers: this.header } : { method: method, headers: this.header };
   }
 
-  public async registerUser(body: {
-    username: string
-    email: string
-    password: string
-  }) {
-    const reqHeader = this.getHeader()
-    const fetchOption = this.getFetchOptions(reqHeader, "POST", body)
-    const URL = "/api/auth/register"
-    const res = await fetch(URL, fetchOption)
-    return res
+  // Authorization
+  public async registerUser(body: { username: string; email: string; password: string }) {
+    const fetchOption = this.getFetchOptions("POST", body);
+    const URL = "/api/auth/register";
+    const res = await fetch(URL, fetchOption);
+    return res;
   }
 
   public async loginUser(body: { email: string; password: string }) {
-    const header = this.getHeader()
-    const fetchOption = this.getFetchOptions(header, "POST", body)
-    const URL = "/api/auth/login"
-    const res = await fetch(URL, fetchOption)
-    return res
+    const fetchOption = this.getFetchOptions("POST", body);
+    const URL = "/api/auth/login";
+    const res = await fetch(URL, fetchOption);
+    return res;
   }
 
   public async logoutUser() {
-    const header = this.getHeader()
-    const fetchOption = this.getFetchOptions(header, "POST")
-    const URL = "/api/auth/logout"
-    const res = await fetch(URL, fetchOption)
-    return res
+    const fetchOption = this.getFetchOptions("POST");
+    const URL = "/api/auth/logout";
+    const res = await fetch(URL, fetchOption);
+    return res;
+  }
+
+  // Post
+  public async createPost(body: { title: Post["title"]; content: Post["content"] }) {
+    const fetchOption = this.getFetchOptions("POST", body);
+    const URL = "/api/post";
+    const res = await fetch(URL, fetchOption);
+    return res;
   }
 }
